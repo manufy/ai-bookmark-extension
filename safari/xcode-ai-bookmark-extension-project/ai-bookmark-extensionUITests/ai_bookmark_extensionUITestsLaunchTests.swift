5b4cd9abedddd8c8f6c321aeea48e80ed87ca450
Manu <Manuel.Fernandez.Yanez@ibm.com>
2024-09-05 16:13:50 +0200

    XCode project for safari added

---
+++ safari/xcode-ai-bookmark-extension-project/ai-bookmark-extensionUITests/ai_bookmark_extensionUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ai_bookmark_extensionUITestsLaunchTests.swift
+//  ai-bookmark-extensionUITests
+//
+//  Created by Manu on 5/9/24.
+//
+
+import XCTest
+
+final class ai_bookmark_extensionUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
